97b664938a3e9e45068e3b899b9016aa4e598bd7
Rostislav1986 <SorryRost@yandex.ru>
2022-09-28 22:04:57 +0300

    1v

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -4,7 +4,9 @@ import hiber.model.User;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 import java.util.List;
 
@@ -25,5 +27,18 @@ public class UserDaoImp implements UserDao {
       TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
       return query.getResultList();
    }
+
+   @Override
+   public User getUserByCar(String model, int series) {
+         TypedQuery<User> query = sessionFactory.getCurrentSession()
+                 .createQuery("from User user where user.car.model=:model and user.car.series=:series", User.class)
+                 .setParameter("model", model)
+                 .setParameter("series", series);
+         return query.getSingleResult();
+      }
+
+
+
+
 
 }
